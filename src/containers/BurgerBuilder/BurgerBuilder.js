383a58a4898d1e8196dff2f8f7b4215356d2ad70
yokoto <youkotoushi@gmail.com>
2020-01-03 02:44:40 +0900

    Add navigation to Checkout page using this.props.history.push('/checkout')

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -32,6 +32,7 @@ class BurgerBuilder extends Component {
 
 
   componentDidMount () {
+    console.log(this.props);
     axios.get('https://burger-builder-app-7f75a.firebaseio.com/ingredients.json')
       .then(response => {
         this.setState({ingredients: response.data});
@@ -98,29 +99,30 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    // alert('You continue!');
-    this.setState({loading: true});
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Toshi Yokota',
-        address: {
-          street: 'Tsubameshi brabra13',
-          zipCode: '9590235',
-          country: 'Japan'
-        },
-        email: 'test@gmail.com'
-      },
-      deliveryMethod: 'fastest'
-    }
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    // // alert('You continue!');
+    // this.setState({loading: true});
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Toshi Yokota',
+    //     address: {
+    //       street: 'Tsubameshi brabra13',
+    //       zipCode: '9590235',
+    //       country: 'Japan'
+    //     },
+    //     email: 'test@gmail.com'
+    //   },
+    //   deliveryMethod: 'fastest'
+    // }
+    // axios.post('/orders.json', order)
+    //   .then(response => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   })
+    //   .catch(error => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   });
+    this.props.history.push('/checkout');
   }
 
   render() {
