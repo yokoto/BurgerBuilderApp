0877fd3017265ac26758b737e44a0db5d013b5b2
yokoto <youkotoushi@gmail.com>
2020-01-04 02:21:26 +0900

    Add outputting the orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -20,7 +20,7 @@ class Orders extends Component {
             id: key
           });
         }
-        this.setState({loading: false})
+        this.setState({loading: false, orders: fetchedOrders})
       })
       .catch(err => {
         this.setState({loading: false})
@@ -30,8 +30,12 @@ class Orders extends Component {
   render () {
     return (
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map(order =>(
+          <Order
+            key={order.id}
+            ingredients={order.ingredients}
+            price={order.price} />
+        ))}
       </div>
     );
   }
