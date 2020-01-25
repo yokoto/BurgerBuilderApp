30596cde992044dfa13fac625f7039dc14d60328
yokoto <yo44ko55@gmail.com>
2020-01-25 11:00:56 +0900

    Add applyMiddleware to execute asynchronous code

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -25,13 +25,13 @@ class BurgerBuilder extends Component {
 
   componentDidMount () {
     console.log(this.props);
-    // axios.get('https://burger-builder-app-7f75a.firebaseio.com/ingredients.json')
-    //   .then(response => {
-    //     this.setState({ingredients: response.data});
-    //   })
-    //   .catch(error => {
-    //     this.setState({error: true})
-    //   });
+    axios.get('https://burger-builder-app-7f75a.firebaseio.com/ingredients.json')
+      .then(response => {
+        this.setState({ingredients: response.data});
+      })
+      .catch(error => {
+        this.setState({error: true})
+      });
   }
 
   updatePurchaseState (ingredients) {
