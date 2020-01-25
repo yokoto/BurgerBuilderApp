2777d358ce7faf2088b4723749e3a9ede5f22969
yokoto <yo44ko55@gmail.com>
2020-01-25 12:06:05 +0900

    Modify to initialize ingredients in the BurgerBuilder

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -25,8 +25,8 @@ export const setIngredients = (ingredients) => {
 export const fetchIngredientsFailed = () => {
     return {
         type: actionTypes.FETCH_INGREDIENTS_FAILED
-    }
-}
+    };
+};
 
 export const initIngredients = () => {
     return dispatch => {
